97a5f7145801a450fad41889eb25f3194dcf1965
Rupam Mitra <rupammitra@Rupams-MacBook-Pro.local>
2016-08-21 00:51:24 +0530

    Small fix.

--- RMPageControlDemo/ViewController.swift
+++ RMPageControlDemo/ViewController.swift
@@ -56,9 +56,9 @@ extension UIColor {
     
     static func randomColor() -> UIColor? {
         
-        let randomRed:CGFloat = CGFloat(drand48())
-        let randomGreen:CGFloat = CGFloat(drand48())
-        let randomBlue:CGFloat = CGFloat(drand48())
+        let randomRed: CGFloat = CGFloat(drand48())
+        let randomGreen: CGFloat = CGFloat(drand48())
+        let randomBlue: CGFloat = CGFloat(drand48())
         return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
     }
 }
